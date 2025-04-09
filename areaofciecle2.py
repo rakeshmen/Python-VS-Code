e39dfe9680a69b315f2c8d6fa5753b80178dd808
Rakesh Men <rakeshmen@outlook.com>
2025-04-09 16:19:54 +0530

    Correction

--- areaofciecle2.py
+++ areaofciecle2.py
@@ -2,18 +2,17 @@
 
 from math import pi
 
-# r = input("Enter Radius of the Circle: ")
+r = input("Hi Rakesh, Enter Radius of the Circle: ")
 
 try:
-    r = float(input("Enter Radius of the Circle: "))
-    #    r = float(r)
+    r = float(r)
     if r > 0:
         area = pi * r * r
-        print("Area of circle is", round(area, 2))
+        print("Area of circle is", round(area, 2), "Square Units.")
     else:
-        print(f"You have Entered '{r}', Radius can not be Negative.")
+        print(f"You have Entered '{r}', Radius can not be 0 or Negative.")
 except ValueError:
     print(
-        f"You have entered '{r}'.\nPlease Enter Radius in Positive Integer or Decimal Fraction"
+        f"You have entered '{r}'.\nEnter Radius in Positive Integer or Decimal Fraction."
     )
-# End of file
+# End of file - Rakesh Men
